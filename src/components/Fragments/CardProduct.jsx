2c0157308440e8266252133d0a93bbe1af2848b0
poorgrammer <poor.grammer.code@gmail.com>
2024-02-11 00:01:12 +0700

    learn: Rendering list

--- src/components/Fragments/CardProduct.jsx
+++ src/components/Fragments/CardProduct.jsx
@@ -5,8 +5,8 @@
 const CardProduct = (props) => {
   const { children } = props;
   return (
-    <div className="max-w-xs bg-white w-full border border-gray-200 rounded-lg shadow m-2">
-      <div className="p-5">{children}</div>
+    <div className="max-w-xs bg-white w-full border border-gray-200 rounded-lg shadow m-2 flex flex-col justify-between">
+      {children}
     </div>
   );
 };
@@ -14,8 +14,8 @@ const CardProduct = (props) => {
 const HeaderCardProduct = (props) => {
   const { image } = props;
   return (
-    <a href="">
-      <img src={image} alt="" />
+    <a href="" className="w-full p-5">
+      <img src={image} alt="" className="w-9/12 mx-auto" />
     </a>
   );
 };
@@ -23,19 +23,21 @@ const HeaderCardProduct = (props) => {
 const BodyCardProduct = (props) => {
   const { productName, children } = props;
   return (
-    <a href="">
-      <h5 className="mt-4 mb-2 text-lg font-bold tracking-tight text-gray-700">
-        {productName}
-      </h5>
-      <p className="font-normal text-sm text-gray-700">{children}</p>
-    </a>
+    <div className="px-5 pb-5 h-full">
+      <a href="">
+        <h5 className="mt-4 mb-2 text-lg font-bold tracking-tight text-gray-700">
+          {productName}
+        </h5>
+        <p className="font-normal text-sm text-gray-700">{children}</p>
+      </a>
+    </div>
   );
 };
 
 const FooterCardProduct = (props) => {
   const { price } = props;
   return (
-    <div className="flex justify-between pt-6 items-center">
+    <div className="flex justify-between items-center px-5 pb-5">
       <h3 className="text-xl font-bold text-rose-600">{price}</h3>
       <a
         href=""
--- src/pages/products/index.jsx
+++ src/pages/products/index.jsx
@@ -1,25 +1,38 @@
 import CardProduct from "../../components/Fragments/CardProduct";
 
+//Data dummy untuk test rendering list
+const products = [
+  {
+    id: 1,
+    name: "Crew Neck",
+    image: "/images/produk-1.jpg",
+    price: "Rp. 450.000",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur explicabo tempore autem amet corporis repudiandae.",
+  },
+  {
+    id: 2,
+    name: "New Crew Neck",
+    image: "/images/produk-1.jpg",
+    price: "Rp. 700.000",
+    description: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur
+      explicabo tempore autem amet corporis repudiandae. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.`,
+  },
+];
+
 const ProductsPage = () => {
   return (
     <div className="flex justify-center">
-      <CardProduct>
-        <CardProduct.Header image="/images/produk-1.jpg" />
-        <CardProduct.Body productName="Crew Neck">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur
-          explicabo tempore autem amet corporis repudiandae.
-        </CardProduct.Body>
-        <CardProduct.Footer price="Rp. 450.000" />
-      </CardProduct>
-
-      <CardProduct>
-        <CardProduct.Header image="/images/produk-1.jpg" />
-        <CardProduct.Body productName="Crew Neck">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur
-          explicabo tempore autem amet corporis repudiandae.
-        </CardProduct.Body>
-        <CardProduct.Footer price="Rp. 450.000" />
-      </CardProduct>
+      {products.map((product) => (
+        //key disini bukan merupakan props
+        <CardProduct key={product.id}>
+          <CardProduct.Header image={product.image} />
+          <CardProduct.Body productName={product.name}>
+            {product.description}
+          </CardProduct.Body>
+          <CardProduct.Footer price={product.price} />
+        </CardProduct>
+      ))}
     </div>
   );
 };
